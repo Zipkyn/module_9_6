f694a3274f9f43c36bac4d15ed278b12b4d36723
Zipkyn <v.t.s.782003@gmail.com>
2024-10-20 19:28:41 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,14 @@
+def all_variants(text):
+
+    for length in range(1, len(text) + 1):
+      
+        for i in range(len(text) - length + 1):
+            yield text[i:i + length]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+
+
+
